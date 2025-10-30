dec0fc4f9b5cfcf4f286ff94fecf758ee09b9cf9
Mischa Sasse <mmischa1@gmail.com>
2025-10-31 00:25:05 +0100

    added a little more once again

--- 3.0-lists-dicts/3.0-lists-dicts-A.py
+++ 3.0-lists-dicts/3.0-lists-dicts-A.py
@@ -1,5 +1,6 @@
 myList = [] # houdt een order aan
 myDict = {} # houdt een kvp aan (key-value pair)
+# beide zijn mutable (ze kunnen veranderen) tov oa een string
 
 fruits = []
 
@@ -8,6 +9,15 @@ myFruit = "Cactus fruit"
 fruits.append('Apple')
 fruits.append('Banana')
 fruits.append(myFruit)
+
+fruits.insert(1, "lemon")
+
+# je kan ook .extend gebruiken om dingen van een list2 toe te voegen aan list1, maar kan ook voor tuples, sets, ect
+myTuple = ("orange", "kiwi")
+# een tuple wordt gebruikt om meerdere items in 1 variabel te bewaren (also tuples kan je niet aanpassen)
+
+print(myTuple[0])
+fruits.extend(myTuple)
 
 print(fruits)
 print(fruits[1])
@@ -19,6 +29,12 @@ print("---------------")
 myPerson = {'name': 'John', 'age': 20, 'email': 'john@doe.com'}
 print(myPerson)
 personsAge = myPerson['age']
-
-# myPerson['age'] = 30
 print(personsAge)
+
+myPerson.update({'job': 'chef'})
+myPerson['country'] = 'The Netherlands'
+myPerson = {**myPerson, 'city': 'Almere'}
+myPerson = dict(myPerson, happyWithMajor= 'No')
+# 4 manieren om iets toe te voegen aan een dict
+
+print(myPerson)
